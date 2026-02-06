c546f476525510bbae093bc6de6712d81fbc061c
aengus <oleg.durasov.2016@gmail.com>
2026-02-06 23:53:23 +0200

    bump version to 3 (1.1.1)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.aengussong.seamcarver"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
