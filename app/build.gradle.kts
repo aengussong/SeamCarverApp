a2ddb12cf28bacac56d2dd79adda80a63cf1a629
aengus <oleg.durasov.2016@gmail.com>
2026-02-06 23:10:03 +0200

    update targetSdk to 35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.aengussong.seamcarver"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.aengussong.seamcarver"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 2
         versionName = "1.1"
 
